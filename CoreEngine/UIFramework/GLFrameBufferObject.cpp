6d3c487f3d721b8e1b132aa18e6c64db6acb4e0a
linh.la <lamanhlinh2000@yahoo.com>
2022-09-04 03:24:05 +0700

    [UIFramework][GLFrameBufferObject] Update support init DepthMap buffer

--- CoreEngine/UIFramework/GLFrameBufferObject.cpp
+++ CoreEngine/UIFramework/GLFrameBufferObject.cpp
@@ -32,17 +32,19 @@ GLFrameBufferObject::~GLFrameBufferObject()
  * @param      pRenderer     The renderer
  * @param[in]  texture_path  The texture path
  */
-GLFrameBufferObject::GLFrameBufferObject(std::string name, int x, int y, int width, int height) :m_name(name)
+GLFrameBufferObject::GLFrameBufferObject(std::string name, int width, int height) :m_name(name)
 {
-	m_viewport = glm::i32vec4(x, y, width, height);
+	m_resolution = glm::i32vec2(width, height);
 }
 
-GLFrameBufferObject::GLFrameBufferObject(std::string name, glm::i32vec4 viewport) : m_name(name), m_viewport(viewport)
-{}
+GLFrameBufferObject::GLFrameBufferObject(std::string name, glm::i32vec4 viewport) : m_name(name)
+{
+	m_resolution = glm::i32vec2(viewport[2], viewport[3]);
+}
 
 bool GLFrameBufferObject::isInit()
 {
-	return ((m_pTexture) && (m_pTexture->getID() != 0) && (m_framebuffer != 0));
+	return ((nullptr != m_pTexture) && (m_pTexture->getID() != 0) && (m_framebuffer != 0));
 }
 
 void GLFrameBufferObject::initTexture2D(GLenum attachment)
@@ -64,10 +66,10 @@ void GLFrameBufferObject::initTexture2D(GLenum attachment)
 	switch (attachment)
 	{
 	case GL_COLOR_ATTACHMENT0:
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_viewport[2], m_viewport[3], 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_resolution[0], m_resolution[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 		break;
 	case GL_DEPTH_STENCIL_ATTACHMENT:
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_viewport[2], m_viewport[3], 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_resolution[0], m_resolution[1], 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
 		break;
 	default:
 		break;
@@ -82,9 +84,13 @@ void GLFrameBufferObject::initTexture2D(GLenum attachment)
 	// attach it to currently bound framebuffer object
 	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, textureColorbuffer, 0);
 
+	// Set the list of draw buffers.
+	GLenum DrawBuffers[1] = {attachment};
+	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+
 	glGenRenderbuffers(1, &m_rbo);
 	glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_viewport[2], m_viewport[3]);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_resolution[0], m_resolution[1]);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
 
@@ -102,7 +108,7 @@ void GLFrameBufferObject::initTexture2DMS(GLenum attachment, GLsizei sample)
 {
 	if (isInit())
 	{
-		LOG_OFF("Frame buffer MSAA already init");
+		LOG_OFF("Frame buffer already init");
 		return;
 	}
 
@@ -110,20 +116,24 @@ void GLFrameBufferObject::initTexture2DMS(GLenum attachment, GLsizei sample)
 	glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
 
 	// generate texture
-	unsigned int texturebufferMs = 0U;
-	glGenTextures(1, &texturebufferMs);
-	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, texturebufferMs);
+	unsigned int textureColorbuffer = 0U;
+	glGenTextures(1, &textureColorbuffer);
+	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorbuffer);
 	// Give an empty image to OpenGL ( the last "0" )
-	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, sample, GL_RGB, m_viewport[2], m_viewport[3], GL_TRUE);
+	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, sample, GL_RGBA, m_resolution[0], m_resolution[1], GL_TRUE);
 
 	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
 
 	// attach it to currently bound framebuffer object
-	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D_MULTISAMPLE, texturebufferMs, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D_MULTISAMPLE, textureColorbuffer, 0);
+	
+	// Set the list of draw buffers.
+	GLenum DrawBuffers[1] = {attachment};
+	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
 
 	glGenRenderbuffers(1, &m_rbo);
 	glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
-	glRenderbufferStorageMultisample(GL_RENDERBUFFER, sample, GL_DEPTH24_STENCIL8, m_viewport[2], m_viewport[3]);
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, sample, GL_DEPTH24_STENCIL8, m_resolution[0], m_resolution[1]);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
 
@@ -134,7 +144,51 @@ void GLFrameBufferObject::initTexture2DMS(GLenum attachment, GLsizei sample)
 	}
 	glBindFramebuffer(GL_FRAMEBUFFER, GLRenderFBOManipulator::top());
 
-	m_pTexture = GLTexture::create(m_name, texturebufferMs);
+	m_pTexture = GLTexture::create(m_name, textureColorbuffer);
+}
+
+
+void GLFrameBufferObject::initDepthMap(GLenum attachment)
+{
+	if (isInit())
+	{
+		LOG_OFF("Frame buffer already init");
+		return;
+	}
+
+	glGenFramebuffers(1, &m_framebuffer);
+
+	// generate texture
+	unsigned int depthMapTexture = 0U;
+	glGenTextures(1, &depthMapTexture);
+	glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+	// Give an empty image to OpenGL ( the last "0" )
+	glTexImage2D(GL_TEXTURE_2D, 0, attachment, m_resolution[0], m_resolution[1], 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	
+	// set texture options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// attach it to currently bound framebuffer object
+	glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMapTexture, 0);
+
+	//  explicitly tell OpenGL we're not going to render any color data
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		LOG_DEBUG("fail to attached frame buffer");
+		_ASSERT(false);
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, GLRenderFBOManipulator::top());
+
+	m_pTexture = GLTexture::create(m_name, depthMapTexture);
 }
 
 std::string GLFrameBufferObject::getName() const
--- CoreEngine/UIFramework/inc/GLFrameBufferObject.h
+++ CoreEngine/UIFramework/inc/GLFrameBufferObject.h
@@ -18,12 +18,12 @@ private:
 	GLuint m_framebuffer = 0U;
 	GLuint m_rbo = 0U;
 
-	glm::i32vec4 m_viewport;
+	glm::i32vec2 m_resolution;
 
 	GLTexturePtr m_pTexture = nullptr;
 protected:
 	friend class creator<GLFrameBufferObject>;
-	GLFrameBufferObject(std::string, int x, int y, int width, int height);
+	GLFrameBufferObject(std::string, int width, int height);
 	GLFrameBufferObject(std::string, glm::i32vec4);
 
 	bool isInit();
@@ -31,14 +31,13 @@ public:
 	~GLFrameBufferObject();
 	void initTexture2D(GLenum  attachment = GL_COLOR_ATTACHMENT0);
 	void initTexture2DMS(GLenum  attachment = GL_COLOR_ATTACHMENT0, GLsizei sample = 4);
+	void initDepthMap(GLenum attachment = GL_DEPTH_COMPONENT16);
 
 	std::string getName() const;
 	GLuint getID() const;
 
 	GLuint getTextureID() const;
 	GLTexturePtr getTexture();
-
-	void bindTexture(std::string);
 };
 
 typedef std::shared_ptr<GLFrameBufferObject> GLFrameBufferObjectPtr;
