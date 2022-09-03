d39dfbda5c3e2cd12816217f1c96fa89cdb828a7
linh.la <lamanhlinh2000@yahoo.com>
2022-09-04 03:26:53 +0700

    [UIFramework][Light] Implement support class for light view matrix

--- CoreEngine/UIFramework/inc/NodeLight.h
+++ CoreEngine/UIFramework/inc/NodeLight.h
@@ -2,6 +2,51 @@
 #include "creator.h"
 #include <UIObject.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+template<class L>
+class LightHelper
+{
+private:
+	std::shared_ptr<L> m_pLight = nullptr;
+public:
+	LightHelper(UIObjectPtr p)
+	{
+		assert(p);
+		m_pLight = std::dynamic_pointer_cast<L>(p);
+	}
+	~LightHelper() {}
+
+	glm::mat4 getViewMatrix(glm::vec3 target, glm::vec3 up)
+	{
+		return glm::lookAt(getPosition(), target, up);
+	}
+
+
+	glm::vec3 getPosition()
+	{
+		glm::vec3 position;
+		switch (m_pLight->GetPropertyValue<glm::i32>(LIGHT_TYPE))
+		{
+		case POINT_LIGHT:
+			position = m_pLight->GetPropertyValue<glm::vec3>(POINT_LIGHT_POSITION);
+			break;
+		case DIRECTIONAL_LIGHT:
+			position = m_pLight->GetPropertyValue<glm::vec3>(DIRECTIONAL_LIGHT_POSITION);
+			break;
+		case SPOT_LIGHT:
+			position = m_pLight->GetPropertyValue<glm::vec3>(SPOT_LIGHT_POSITION);
+			break;
+		default:
+			break;
+		}
+		//<get light position in world
+		return position;
+	}
+};
+
 class NodePointLight final: public UIObject, public creator<NodePointLight>
 {
 protected:
